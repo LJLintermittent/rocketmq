c3b5c0722cdf796a5248fd8c49f29fb98e2a73d9
lijiale <1157221853@qq.com>
2021-12-30 11:51:36 +0800

    项目初始化

---
+++ src/main/java/com/learn/rocketmq/RocketmqApplication.java
@@ -0,0 +1,14 @@
+package com.learn.rocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@SuppressWarnings("all")
+public class RocketmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqApplication.class, args);
+    }
+
+}
