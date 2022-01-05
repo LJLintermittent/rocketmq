6eb6cd70cae3c1689fe0814444057b1474f6d147
lijiale <1157221853@qq.com>
2022-01-05 16:23:48 +0800

    rocketmq集群测试发送同步消息

---
+++ src/main/java/com/learn/rocketmq/base/consumer/SyncProducer.java
@@ -0,0 +1,36 @@
+package com.learn.rocketmq.base.consumer;
+
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.client.producer.SendStatus;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Description:
+ * date: 2022/1/1 18:47
+ * Package: com.learn.rocketmq.base.consumer
+ *
+ * @author 李佳乐
+ * @email 18066550996@163.com
+ */
+@SuppressWarnings("all")
+public class SyncProducer {
+
+    public static void main(String[] args) throws MQClientException, RemotingException, InterruptedException, MQBrokerException {
+        DefaultMQProducer mqProducer = new DefaultMQProducer("group1");
+        mqProducer.setNamesrvAddr("192.168.190.136:9876;192.168.190.137:9876");
+        mqProducer.start();
+        for (int i = 0; i < 10; i++) {
+            Message msg = new Message("base", "Tag1", ("Hello world" + i).getBytes());
+            SendResult result = mqProducer.send(msg,1000000);
+            System.out.println("发送结果：" + result);
+            TimeUnit.SECONDS.sleep(1);
+        }
+        mqProducer.shutdown();
+    }
+}
--- src/test/java/com/learn/rocketmq/RocketmqApplicationTests.java
+++ src/test/java/com/learn/rocketmq/RocketmqApplicationTests.java
@@ -1,16 +1,32 @@
 package com.learn.rocketmq;
 
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class RocketmqApplicationTests {
 
     @Test
-    public void contextLoads() {
+    public void SyncProducerTest() throws Exception {
+        DefaultMQProducer producer = new DefaultMQProducer("group1");
+        producer.setNamesrvAddr("192.168.190.136:9876:9876;192.168.190.136:9876:9876");
+        producer.start();
+        for (int i = 0; i < 10; i++) {
+            Message msg = new Message("base", "Tag1", ("Hello world" + i).getBytes());
+            SendResult result = producer.send(msg);
+            System.out.println("发送结果：" + result);
+            TimeUnit.SECONDS.sleep(1);
+        }
+        producer.shutdown();
+
     }
 
 }
